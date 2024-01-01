016042efb0ffede2a50c60923ecaa4875e4377bd
Mohamed Touzani <mohamed.touzani@gmx.de>
2024-01-01 09:00:00 +0000

    feat(core): isAboveGround, isJumping, Kollisionscheck und Trefferlogik

--- models/movable.object.class.js
+++ models/movable.object.class.js
@@ -10,9 +10,6 @@ class MovableObject extends DrawableObject {
   isAlive = true;
   world;
 
-  /**
-   * The function applies gravity to an object by decreasing its vertical position and speed over time.
-   */
   applyGravity() {
     setInterval(() => {
       if (this.isAboveGround() || this.speedY > 0) {
@@ -20,6 +17,75 @@ class MovableObject extends DrawableObject {
         this.speedY -= this.acceleration;
       }
     }, 1000 / 25);
+  }
+
+  /**
+   * The function checks if an object is above the ground or not.
+   */
+  isAboveGround() {
+    if (this instanceof ThrowableObject) {
+      return true;
+    } else {
+      return this.y < this.ground;
+    }
+  }
+
+  /**
+   * The function checks if an object is jumping by comparing its current height to the ground and its
+   * vertical speed.
+   */
+  isJumping() {
+    return this.y - this.ground < 0 && this.speedY < 0;
+  }
+
+  /**
+   * The function checks if two objects are colliding based on their positions and dimensions.
+   */
+  isColliding(obj) {
+    return (
+      (obj.isAlive || obj instanceof Coin || obj instanceof Bottle) &&
+      this.x + this.width - this.offset.right >= obj.x + obj.offset.left / 2 &&
+      this.y + this.height > obj.y + obj.offset.top &&
+      this.x + this.offset.left - obj.width / 2 <
+        obj.x + obj.width - obj.offset.right &&
+      this.y + this.offset.top < obj.y + obj.height
+    );
+  }
+
+  /**
+   * The function checks if an object has been jumped on by another object.
+   */
+  isJumpedOn(obj) {
+    return (
+      obj.isAlive &&
+      this.y + this.height > obj.y + obj.offset.top &&
+      this.y + this.offset.top < obj.y + obj.height
+    );
+  }
+
+  /**
+   * The "hit" function reduces the energy and updates lastHit.
+   */
+  hit(damage) {
+    this.energy -= damage;
+    if (this.energy < 0) {
+      this.energy = 0;
+    } else {
+      this.lastHit = new Date().getTime();
+    }
+  }
+
+  /**
+   * Recently hurt?
+   */
+  isHurt() {
+    let timepassed = new Date().getTime() - this.lastHit;
+    timepassed = timepassed / 1000;
+    return timepassed < 1;
+  }
+
+  isDead() {
+    return this.energy == 0;
   }
 
   moveLeft() { this.x -= this.speed; }
