bb916b9d8ea49c731b7da3adad54476b1e4ca465
Mohamed Touzani <mohamed.touzani@gmx.de>
2024-01-01 09:00:00 +0000

    feat(background): füge Cloud-Bilder und Standardgeschwindigkeit hinzu

--- models/cloud.class.js.js
+++ models/cloud.class.js.js
@@ -1,9 +1,14 @@
 class Cloud extends MovableObject {
   height = 360;
   width = 600;
+  speed = 0.5;
+  IMAGES = [
+    "img/5_background/layers/4_clouds/1.png",
+    "img/5_background/layers/4_clouds/2.png",
+  ];
 
-  constructor(x) {
-    super();
+  constructor(x, i) {
+    super().loadImage(this.IMAGES[i]);
     this.x = x;
     this.y = 20;
   }
