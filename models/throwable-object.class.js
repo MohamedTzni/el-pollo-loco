2db15ded7d9828faf1857357e3d4782e14dcc3b5
Mohamed Touzani <mohamed.touzani@gmx.de>
2024-01-01 09:00:00 +0000

    feat(throwable): implementiere throw(direction) mit Gravitation

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -4,4 +4,22 @@ class ThrowableObject extends MovableObject {
   constructor() {
     super();
   }
+
+  /**
+   * The function throws the character in a given direction and applies gravity.
+   * @param direction - The direction in which the object is being thrown. It can be either "right" or
+   * "left".
+   */
+  throw(direction) {
+    this.applyGravity();
+    if (direction === "right") {
+      setInterval(() => {
+        this.x += 15;
+      }, 25);
+    } else if (direction === "left") {
+      setInterval(() => {
+        this.x -= 15;
+      }, 25);
+    }
+  }
 }
