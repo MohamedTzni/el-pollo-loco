684b78fd5049cbcbf5cbafa1578d7af942fef531
Mohamed Touzani <mohamed.touzani@gmx.de>
2024-01-01 09:00:00 +0000

    feat(enemy): füge Walking- & Dead-Sprites hinzu

--- models/smallchicken.class.js
+++ models/smallchicken.class.js
@@ -8,6 +8,15 @@ class Smallchicken extends MovableObject {
     left: 10,
     right: 5,
   };
+  IMAGES_WALKING = [
+    "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+  ];
+  IMAGES_DEAD = [
+    "img/3_enemies_chicken/chicken_small/2_dead/dead.png",
+    "img/3_enemies_chicken/chicken_small/2_dead/dead.png",
+  ];
 
   constructor(x) {
     super();
