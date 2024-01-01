90f343da8e8989a3fbba6511b63b0cb6a0d7c669
Mohamed Touzani <mohamed.touzani@gmx.de>
2024-01-01 09:00:00 +0000

    feat(ui): erstelle CoinBar mit Position und Sprites

---
+++ models/coinbar-class.js
@@ -0,0 +1,17 @@
+class CoinBar extends StatusBar {
+  x = 10;
+  y = 50;
+  IMAGES = [
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/0.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/20.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/40.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/60.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/80.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/100.png",
+  ];
+
+  constructor() {
+    super().loadImages(this.IMAGES);
+    this.setPercentage(0);
+  }
+}
