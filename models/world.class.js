915faa2387a34b0a3b88825ef6f17b9eb31db8ca
Mohamed Touzani <mohamed.touzani@gmx.de>
2024-01-01 09:00:00 +0000

    feat(world): implementiere Item-Sammeln, StatusBars und Flaskenwurf

--- models/world.class.js
+++ models/world.class.js
@@ -1,55 +1,58 @@
 class World {
   /* ... */
 
-  checkCollisions() {
-    this.checkEnemyCollision();
-    this.checkEndbossCollision();
-    this.checkEndbossBottleCollision();
-    this.checkBottleGroundCollison();
-    this.checkBottleIsBroken();
-  }
-
-  checkEnemyCollision() {
-    this.level.enemies.forEach((enemy) => {
-      if (this.isJumpingOnEnemy(enemy)) {
-        enemy.isKilled(enemy);
-        this.character.bounceUp();
-        this.playSound(this.chickenHurt_sound);
+  checkCollection() {
+    this.level.collectableItems.forEach((item) => {
+      if (this.canCollectItem(item, Bottle)) {
+        item.collectItem(item);
+        this.collectBottle();
       }
-      if (this.character.isColliding(enemy)) {
-        this.hitCharacter(5);
+      if (this.canCollectItem(item, Coin)) {
+        item.collectItem(item);
+        this.collectCoin();
       }
     });
   }
 
-  checkEndbossCollision() {
-    this.level.endboss.forEach((endboss) => {
-      if (this.character.isColliding(endboss)) {
-        this.hitCharacter(20);
-      }
-    });
+  canCollectItem(item, Obj) {
+    return this.character.isColliding(item) && item instanceof Obj && !item.isCollected;
   }
 
-  checkCharacterIsLeftOfEndboss() {
-    if (this.isBehindEndboss()) {
-      this.hitCharacter(100);
-    }
+  collectBottle() {
+    this.collectedBottles++;
+    this.playSound(this.bottle_pickup_sound);
+    this.statusBar[2].setPercentage((this.collectedBottles / amountCollectableBottles) * 100);
   }
 
-  isJumpingOnEnemy(enemy) {
-    return this.character.isJumping() && this.character.isColliding(enemy);
-  }
-
-  isBehindEndboss() {
-    return (
-      this.level.endboss[0].x + this.level.endboss[0].width <
-      this.character.x + this.character.width
+  collectCoin() {
+    this.collectedCoins++;
+    this.playSound(this.coin_sound);
+    this.statusBar[1].setPercentage(
+      (this.collectedCoins / amountCollectableCoins) * 100
     );
   }
 
-  hitCharacter(damage) {
-    this.character.hit(damage);
-    this.playSound(this.character.hurt_sound);
-    this.statusBar[0].setPercentage(this.character.energy);
+  checkThrowObjects() {
+    if (this.isBottleAvailabe() && this.character.isLookingLeft()) {
+      let bottle = new ThrowableBottle(this.character.x - 25, this.character.y + 100, "left");
+      this.throwBottle(bottle);
+    } else if (this.isBottleAvailabe()) {
+      let bottle = new ThrowableBottle(this.character.x + 100, this.character.y + 100, "right");
+      this.throwBottle(bottle);
+    }
+  }
+
+  isBottleAvailabe() {
+    return this.keyboard.KEY_D && this.collectedBottles > 0;
+  }
+
+  throwBottle(bottle) {
+    this.throwableObjects.push(bottle);
+    this.collectedBottles--;
+    this.statusBar[2].setPercentage((this.collectedBottles / 8) * 100);
+  }
+
+  getIndexOfItem(array, item) {
+    return array.indexOf(item, 0);
   }
 }
