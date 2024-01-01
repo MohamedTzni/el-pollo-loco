0581fd8b597c6f96a859024a39ea8b7b86a093f1
Mohamed Touzani <mohamed.touzani@gmx.de>
2024-01-01 09:00:00 +0000

    feat(collectable): füge Kollisionsoffsets und Flaschenbild hinzu

--- models/collectable-bottle.class.js
+++ models/collectable-bottle.class.js
@@ -2,9 +2,15 @@ class Bottle extends CollectableObject {
   height = 75;
   width = 75;
   y = 345;
+  offset = {
+    top: 15,
+    bottom: 25,
+    left: 40,
+    right: 20,
+  };
 
   constructor(x) {
-    super();
+    super().loadImage("img/6_salsa_bottle/2_salsa_bottle_on_ground.png");
     this.x = x;
   }
 }
