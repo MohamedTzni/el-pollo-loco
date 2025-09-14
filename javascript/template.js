26c00a3af1806b26d71aee6c14e7685e27e99ff9
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-09-14 20:11:22 +0200

    chore(templates): parity mit geliefertem Stand

--- javascript/template.js
+++ javascript/template.js
@@ -30,21 +30,38 @@ function hideGameUI() {
 
 /**
  * This function renders a random game over screen with a button to reload the game.
- * Picks an index from 0..gameOverScreens.length-1.
+ * @returns a string that contains an HTML `img` element with a randomly selected image from an array
+ * of game over screens, and a `div` element with a "Back to Start" button that, when clicked, will
+ * reload the game. The class of the `img` element is determined by the value of the `endScreenClass`
+ * variable, which is set based on the value
  */
 function renderRandomGameOverScreen() {
-  const endScreenClass = isFullScreen ? "fullendscreen" : "defaultendscreen";
-  const maxIndex = gameOverScreens.length - 1;
-  const randomIndex = Math.floor(Math.random() * (maxIndex + 1));
-  return `<img src="${gameOverScreens[randomIndex]}" class="${endScreenClass}"></img>
+  let endScreenClass;
+  if (isFullScreen) {
+    endScreenClass = "fullendscreen";
+  } else {
+    endScreenClass = "defaultendscreen";
+  }
+  let max = gameOverScreens.length - 1;
+  let randomScreen = Math.floor(Math.random() * (max - +1));
+  return `<img src="${gameOverScreens[randomScreen]}" class="${endScreenClass}"></img>
     <div class="button flex-center" onclick="reloadGame()">Back to Start</div>`;
 }
 
 /**
  * The function renders a game won screen with an image and a button to reload the game.
+ * @returns a string that contains an HTML code for an image and a button. The image source is
+ * "img/9_intro_outro_screens/game_over/gamewon2.png" and its class is determined by the value of the
+ * variable `endScreenClass`. The button has a class of "button flex-center" and an onclick event that
+ * calls the `reloadGame()` function.
  */
 function renderGameWonScreen() {
-  const endScreenClass = isFullScreen ? "fullendscreen" : "defaultendscreen";
-  return `<img src="img/9_intro_outro_screens/game_over/gamewon2.png" class="${endScreenClass}"></img>
+  let endScreenClass;
+  if (isFullScreen) {
+    endScreenClass = "fullendscreen";
+  } else {
+    endScreenClass = "defaultendscreen";
+  }
+  return `<img src="img/9_intro_outro_screens/game_over/gamewon2.png" class= ${endScreenClass}></img>
     <div class="button flex-center" onclick="reloadGame()">Back to Start</div>`;
-}
+} 
