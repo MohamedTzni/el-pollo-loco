1620aa84c87b6cac127bbfc95700c43daf838960
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-09-14 20:36:07 +0200

    feat(collectable): implementiere collectItem() zum Entfernen von Items aus Level

--- models/collectable-objekt.class.js
+++ models/collectable-objekt.class.js
@@ -5,9 +5,6 @@ class CollectableObject extends DrawableObject {
     super();
   }
 
-  /**
-   * The function pauses a sound with a playback rate of 1.
-   */
   playAudio() {
     this.collect_sound.playbackRate = 1;
     this.collect_sound.pause();
@@ -18,5 +15,19 @@ class CollectableObject extends DrawableObject {
     let path = images[i];
     this.img = this.imageCache[path];
     this.currentImage++;
+  }
+
+  /**
+   * The function collects an item in a game world and removes it from the list of collectable items.
+   * @param item - The "item" parameter is a variable that represents the collectable item that the
+   * player has collected in the game. It could be a coin, a power-up, or any other item that the player
+   * needs to collect to progress in the game.
+   */
+  collectItem(item) {
+    this.isCollected = true;
+    this.collect_sound.playbackRate = 2;
+    world.playSound(this.collect_sound);
+    let indexofitem = world.getIndexOfItem(world.level.collectableItems, item);
+    world.level.collectableItems.splice(indexofitem, 1);
   }
 }
