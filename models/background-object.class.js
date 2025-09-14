0f7473cbdbb4f6602ce7d703662157671f09a66f
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-09-14 20:19:39 +0200

    chore(background): parity mit geliefertem Code

---
+++ models/background-object.class.js
@@ -0,0 +1,10 @@
+class BackgroundObject extends MovableObject {
+  height = 480;
+  width = 720;
+
+  constructor(imagePath, x, y) {
+    super().loadImage(imagePath);
+    this.x = x;
+    this.y = y;
+  }
+}
