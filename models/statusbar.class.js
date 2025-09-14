e19503cff5cc66b213ec567fc8cf835001d90179
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-09-14 22:40:59 +0200

    chore(ui): parity mit geliefertem statusbar.class.js

--- models/statusbar.class.js
+++ models/statusbar.class.js
@@ -7,12 +7,24 @@ class StatusBar extends DrawableObject {
     super();
   }
 
+/**
+ * The function sets the percentage and updates the image accordingly.
+ * @param percentage - The percentage parameter is a number between 0 and 5 that represents the
+ * progress or completion level of a task or process. It is used to update the image displayed by the
+ * code.
+ */
   setPercentage(percentage) {
     this.percentage = percentage; // => 0....5
     let path = this.IMAGES[this.resolveImageIndex()];
     this.img = this.imageCache[path];
   }
 
+  /**
+   * The function returns an image index based on the percentage value.
+   * @returns The function `resolveImageIndex()` returns an integer value between 0 and 5 based on the
+   * value of the `percentage` property of the object calling the function. The value returned indicates
+   * which image should be displayed based on the progress of a task.
+   */
   resolveImageIndex() {
     if (this.percentage == 100) {
       return 5;
