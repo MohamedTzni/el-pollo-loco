b6fca31bb26b25824ba9c0bcad3d8515534eb251
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-09-14 20:08:11 +0200

    feat(overlay): implementiere Öffnen/Schließen-Logik inkl. aria-hidden

--- javascript/overlay.js
+++ javascript/overlay.js
@@ -3,6 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((r) => r.text())
     .then((html) => {
       document.body.insertAdjacentHTML("beforeend", html);
+
+      const btnExplain = document.getElementById("btn-explain");
+      const overlay = document.getElementById("explanationOverlay");
+      const btnClose = document.getElementById("overlay-close");
+
+      btnExplain.addEventListener("click", () => {
+        overlay.classList.add("show");
+        overlay.setAttribute("aria-hidden", "false");
+      });
+
+      const closeOverlay = () => {
+        overlay.classList.remove("show");
+        overlay.setAttribute("aria-hidden", "true");
+      };
+      btnClose.addEventListener("click", closeOverlay);
     })
-    .catch((err) => console.error("[overlay] overlay.html konnte nicht geladen werden:", err));
+    .catch((err) =>
+      console.error("[overlay] overlay.html konnte nicht geladen werden:", err)
+    );
 });
