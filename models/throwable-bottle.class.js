15447ac4e93beb57cadae062835d20e017d3f6ad
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-09-14 22:43:34 +0200

    feat(throwable): implementiere animate() für Flugrotation und Zersplittern

--- models/throwable-bottle.class.js
+++ models/throwable-bottle.class.js
@@ -3,7 +3,6 @@ class ThrowableBottle extends ThrowableObject {
   width = 75;
   ground = 350;
   speedY = 25;
-
   IMAGES_ROTATION = [
     "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
@@ -26,5 +25,19 @@ class ThrowableBottle extends ThrowableObject {
     this.x = x;
     this.y = y;
     this.throw(direction);
+  }
+
+  /**
+   * This function animates images by repeatedly calling the playAnimation function every 50
+   * milliseconds.
+   */
+  animate() {    
+      setInterval(() => {
+        if (!this.isBroken) {
+          this.playAnimation(this.IMAGES_ROTATION);
+        } else {
+          this.playAnimation(this.IMAGES_SPLASH);
+        }
+      }, 50);
   }
 }
