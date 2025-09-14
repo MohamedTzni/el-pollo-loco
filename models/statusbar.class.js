bff8732e38a4af79d550481d394245b15be30a27
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-09-14 22:40:18 +0200

    feat(ui): füge resolveImageIndex() basierend auf Prozentbereichen hinzu

--- models/statusbar.class.js
+++ models/statusbar.class.js
@@ -12,4 +12,20 @@ class StatusBar extends DrawableObject {
     let path = this.IMAGES[this.resolveImageIndex()];
     this.img = this.imageCache[path];
   }
+
+  resolveImageIndex() {
+    if (this.percentage == 100) {
+      return 5;
+    } else if (this.percentage > 70) {
+      return 4;
+    } else if (this.percentage > 50) {
+      return 3;
+    } else if (this.percentage > 30) {
+      return 2;
+    } else if (this.percentage > 1) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 }
