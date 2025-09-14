b3ab28b98a3312f67c752e8851d10d7b0f737352
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-09-14 20:37:36 +0200

    feat(core): füge draw() und drawFrame() hinzu

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -12,4 +12,29 @@ class DrawableObject {
     left: 0,
     right: 0,
   };
+
+  draw(ctx) {
+    try {
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    } catch (e) {
+      // console.warn("Error loading image", this.img);
+    }
+  }
+
+  drawFrame(ctx) {
+    if (
+      this instanceof Pepe ||
+      this instanceof Chicken ||
+      this instanceof Smallchicken ||
+      this instanceof Endboss ||
+      this instanceof Coin ||
+      this instanceof Bottle
+    ) {
+      ctx.beginPath();
+      ctx.lineWidth = "1";
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
+    }
+  }
 }
