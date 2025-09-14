2008ef4c9efecb37e96754bc632305f26ba37348
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-09-14 20:38:42 +0200

    chore(core): parity mit geliefertem drawable-object.class.js

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -13,12 +13,25 @@ class DrawableObject {
     right: 0,
   };
 
+  /**
+   * This function draws an image on a canvas context and logs a warning if there is an error loading
+   * the image.
+   * @param ctx - ctx stands for "context" and refers to the 2D rendering context of a canvas element.
+   * It is used to draw and manipulate graphics on the canvas.
+   */
   draw(ctx) {
     try {
       ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    } catch (e) {}
+    } catch (e) {
+      // console.warn("Error loading image", this.img);
+    }
   }
 
+  /**
+   * The function draws a blue rectangular frame around certain game objects.
+   * @param ctx - ctx stands for "context" and refers to the canvas context on which the frame is being
+   * drawn. It is an object that provides methods and properties for drawing on the canvas.
+   */
   drawFrame(ctx) {
     if (
       this instanceof Pepe ||
@@ -36,6 +49,11 @@ class DrawableObject {
     }
   }
 
+  /**
+   * This function draws a red rectangular hitbox around certain game objects in a canvas context.
+   * @param ctx - ctx stands for "context" and refers to the canvas context on which the hit box will
+   * be drawn. It is typically obtained by calling the getContext() method on a canvas element.
+   */
   drawHitBox(ctx) {
     if (
       this instanceof Pepe ||
@@ -58,11 +76,21 @@ class DrawableObject {
     }
   }
 
+  /**
+   * The function loads an image from a specified path.
+   * @param path - The path parameter is a string that represents the URL or file path of the image
+   * that needs to be loaded.
+   */
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
   }
 
+  /**
+   * The function loads images from an array and caches them using their file paths as keys.
+   * @param arr - an array of image file paths that need to be loaded and cached in the `imageCache`
+   * object.
+   */
   loadImages(arr) {
     arr.forEach((path) => {
       let img = new Image();
@@ -71,6 +99,9 @@ class DrawableObject {
     });
   }
 
+  /**
+   * The function removes the height and width properties of an object.
+   */
   removeItem() {
     this.height = 0;
     this.width = 0;
