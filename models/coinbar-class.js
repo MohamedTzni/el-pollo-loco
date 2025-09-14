d04394ff99a2b89bf24da13f893f43d0ccc21e7a
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-09-14 20:28:25 +0200

    feat(ui): implementiere resolveImageIndex() für CoinBar

--- models/coinbar-class.js
+++ models/coinbar-class.js
@@ -14,4 +14,20 @@ class CoinBar extends StatusBar {
     super().loadImages(this.IMAGES);
     this.setPercentage(0);
   }
+
+  resolveImageIndex() {
+    if (this.percentage == 100) {
+      return 5;
+    } else if (this.percentage > 80) {
+      return 4;
+    } else if (this.percentage > 60) {
+      return 3;
+    } else if (this.percentage > 40) {
+      return 2;
+    } else if (this.percentage > 20) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 }
