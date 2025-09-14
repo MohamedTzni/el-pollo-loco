27d4fd384afa6692fee5ecc048e4d299c54e16ab
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-09-14 20:35:40 +0200

    feat(collectable): implementiere playAnimation() für Collectables

--- models/collectable-objekt.class.js
+++ models/collectable-objekt.class.js
@@ -4,4 +4,16 @@ class CollectableObject extends DrawableObject {
   constructor() {
     super();
   }
+
+  /**
+   * The function plays an animation by cycling through a list of images.
+   * @param images - The parameter "images" is an array of strings that represent the file paths of the
+   * images to be used in the animation.
+   */
+  playAnimation(images) {
+    let i = this.currentImage % images.length;
+    let path = images[i];
+    this.img = this.imageCache[path];
+    this.currentImage++;
+  }
 }
