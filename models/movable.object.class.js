75a26879918fef4ef9dbe6d4bbdffdbfab844c89
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-04-07 17:20:31 +0200

    Fix Pepe hit cooldown so enemies can actually defeat him

--- models/movable.object.class.js
+++ models/movable.object.class.js
@@ -92,7 +92,7 @@ class MovableObject extends DrawableObject {
    */
   hit(damage = 10) {
     const now = new Date().getTime();
-    if (now - this.lastHit < 1200) return false;
+    if (now - this.lastHit < 500) return false;
 
     this.energy -= damage;
     if (this.energy < 0) {
